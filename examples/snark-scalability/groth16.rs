bd0814c793200dcb2f02226ccff69579de69592f
ValarDragon <dojha12@gmail.com>
2020-12-16 00:08:58 -0600

    Warn for unused

--- examples/snark-scalability/groth16.rs
+++ examples/snark-scalability/groth16.rs
@@ -1,26 +1,11 @@
+#![warn(unused)]
 #![deny(
-    unused_import_braces,
-    unused_qualifications,
     trivial_casts,
-    trivial_numeric_casts
-)]
-#![deny(unused_qualifications, variant_size_differences, stable_features)]
-#![deny(
-    non_shorthand_field_patterns,
-    unused_attributes,
-    unused_imports,
-    unused_extern_crates
-)]
-#![deny(
-    renamed_and_removed_lints,
+    trivial_numeric_casts,
+    variant_size_differences,
     stable_features,
-    unused_allocation,
-    unused_comparisons
-)]
-#![deny(
-    unused_must_use,
-    unused_mut,
-    unused_unsafe,
+    non_shorthand_field_patterns,
+    renamed_and_removed_lints,
     private_in_public,
     unsafe_code
 )]
