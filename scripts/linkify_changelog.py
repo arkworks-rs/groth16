efc152dd6d4e781a8cb1b435eb7860d890943608
crStiv <cryptostiv7@gmail.com>
2025-02-25 22:34:55 +0100

    Update linkify_changelog.py

--- scripts/linkify_changelog.py
+++ scripts/linkify_changelog.py
@@ -9,7 +9,7 @@ repo_name = ""
 
 # This script goes through the provided file, and replaces any " \#<number>",
 # with the valid mark down formatted link to it. e.g.
-# " [\#number](https://github.com/arkworks-rs/template/pull/<number>)
+# " [\#number](https://github.com/arkworks-rs/groth16/pull/<number>)
 # Note that if the number is for a an issue, github will auto-redirect you when you click the link.
 # It is safe to run the script multiple times in succession. 
 #
